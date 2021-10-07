beb9fa2e60bae0d9adcbdd89e681e1a96f3656d9
nkitner <nicolekitner@gmail.com>
2021-10-06 21:48:52 -0400

    Update model.py
    not at all what it will be but a start

--- model.py
+++ model.py
@@ -18,21 +18,24 @@ def get_unet(img_size, num_classes):
     ### [First half of the network: downsampling inputs] ###
 
     # Entry block
-    x = layers.Conv2D(2, 3, strides=1, padding="same")(inputs)
+    x = layers.Conv2D(32, 3, strides=1, padding="same")(inputs)
     x = layers.MaxPool2D()(x)
     x = layers.Activation("relu")(x)
 
     previous_block_activation = x  # Set aside residual
 
     # Blocks 1, 2, 3 are identical apart from the feature depth.
-    for filters in [64, 32, 16]:
+    for filters in [64, 128, 256]:
         x = layers.Activation("relu")(x)
         x = layers.Conv2D(filters, 3, padding="same")(x)
         x = layers.MaxPool2D()(x)
 
-        # x = layers.Activation("relu")(x)
-        # x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        # x = layers.BatchNormalization()(x)
+        y = layers.Activation("relu")(x)
+        y = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        y = layers.BatchNormalization()(x)
+
+        # layers.Add used for cfrb
+        y = layers.Add()([x,y])
         #
         # x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
 
@@ -45,7 +48,7 @@ def get_unet(img_size, num_classes):
 
     ### [Second half of the network: upsampling inputs] ###
 
-    for filters in [16, 32, 64]:
+    for filters in [256, 128, 64, 32]:
         x = layers.Activation("relu")(x)
         x = layers.Conv2D(filters, 3, padding="same")(x)
         x = layers.UpSampling2D()(x)
@@ -57,7 +60,7 @@ def get_unet(img_size, num_classes):
         # x = layers.UpSampling2D(2)(x)
 
         # Project residual
-        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.UpSampling2D()(previous_block_activation)
         residual = layers.Conv2D(filters, 1, padding="same")(residual)
         x = layers.add([x, residual])  # Add back residual
         previous_block_activation = x  # Set aside next residual
@@ -69,6 +72,8 @@ def get_unet(img_size, num_classes):
     model = keras.Model(inputs, outputs)
     return model
 
+# def get_kitenet(img_size, num_classes):
+#
 
 # Free up RAM in case the model definition cells were run multiple times
 keras.backend.clear_session()
