05e60260c0fab18c1abce199bc08b05c3797c300
nkitner <nicolekitner@gmail.com>
2021-10-06 21:10:03 -0400

    Update model.py
    first go at creating the correct unet

--- model.py
+++ model.py
@@ -12,29 +12,29 @@ import matplotlib.pyplot as plt
 from tensorflow.keras import layers
 
 
-def get_model(img_size, num_classes):
+def get_unet(img_size, num_classes):
     inputs = keras.Input(shape=img_size + (3,))
 
     ### [First half of the network: downsampling inputs] ###
 
     # Entry block
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
-    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(2, 3, strides=1, padding="same")(inputs)
+    x = layers.MaxPool2D()(x)
     x = layers.Activation("relu")(x)
 
     previous_block_activation = x  # Set aside residual
 
     # Blocks 1, 2, 3 are identical apart from the feature depth.
-    for filters in [64, 128, 256]:
+    for filters in [64, 32, 16]:
         x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+        x = layers.Conv2D(filters, 3, padding="same")(x)
+        x = layers.MaxPool2D()(x)
 
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+        # x = layers.Activation("relu")(x)
+        # x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        # x = layers.BatchNormalization()(x)
+        #
+        # x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
 
         # Project residual
         residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
@@ -45,16 +45,16 @@ def get_model(img_size, num_classes):
 
     ### [Second half of the network: upsampling inputs] ###
 
-    for filters in [256, 128, 64, 32]:
+    for filters in [16, 32, 64]:
         x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+        x = layers.Conv2D(filters, 3, padding="same")(x)
+        x = layers.UpSampling2D()(x)
 
-        x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+        # x = layers.Activation("relu")(x)
+        # x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        # x = layers.BatchNormalization()(x)
 
-        x = layers.UpSampling2D(2)(x)
+        # x = layers.UpSampling2D(2)(x)
 
         # Project residual
         residual = layers.UpSampling2D(2)(previous_block_activation)
@@ -76,5 +76,5 @@ keras.backend.clear_session()
 # Build model
 img_size=(128,128)
 num_classes=3
-model = get_model(img_size, num_classes)
+model = get_unet(img_size, num_classes)
 model.summary()
