03019d26919f289a1289eb31cf9d67964f7e6e3d
nkitner <nicolekitner@gmail.com>
2021-10-07 20:09:00 -0400

    Update model.py
    a mess

--- model.py
+++ model.py
@@ -12,9 +12,6 @@ def convolution_block_unet(input, num_filters):
     x = layers.MaxPool2D((2, 2))(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
-    # x = layers.Conv2D(num_filters, 3, padding="same")(x)
-    # x = layers.BatchNormalization()(x)
-    # x = layers.Activation("relu")(x)
     return x
 
 
@@ -46,19 +43,61 @@ def decoder_block_unet(input, skip_features, num_filters):
     return x
 
 
-def crfb(unet, kinet, scale_factor):
+def decoder_block_kinet(input, num_filters):
+    x = layers.Conv2D(num_filters, 3, padding="same")(input)
+    x = layers.MaxPool2D((2, 2))(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    return x
+
+
+def crfb(x1, x2, num_filters, scale_factor):
+    x2 = layers.Conv2D(num_filters, 3, padding="same")(x2)
+    x2 = layers.BatchNormalization()(x2)
+    x2 = layers.Activation("relu")(x2)
+    x2 = layers.UpSampling2D(size=(scale_factor, scale_factor))(x2)
+    out = layers.Concatenate()([x1, x2])
+    return out
+
+
+def crfb_kinet(unet, kinnet, scale_factor):
     return
 
 
 def kiunet(input_shape):
     inputs = layers.Input(input_shape)
 
-    s1, p1 = encoder_block_unet(inputs, 64)
-    s2, p2 = encoder_block_unet(p1, 128)
-    s3, p3 = encoder_block_unet(p2, 256)
-    s4, p4 = encoder_block_unet(p3, 512)
+    # encoder block
+    s1, p1 = encoder_block_unet(inputs, 16)  # s1 to be used at end
+    k1, j1 = encoder_block_kinet(inputs, 16)  # k1 to be used at end
 
-    b1 = convolution_block_unet(p4, 1024)
+    residual_p1 = convolution_block_kinet(p1, 16)
+    residual_k1 = convolution_block_unet(j1, 16)
+
+    u1 = crfb(p1, residual_k1, 16, 0.25)
+    o1 = crfb(j1, residual_p1, 16, 4)
+
+    s2, p2 = encoder_block_unet(u1, 32)  # s2 to be used second to last
+    k2, j2 = encoder_block_kinet(o1, 32)  # k2 to be used second to last
+
+    residual_p2 = convolution_block_kinet(p2, 32)
+    residual_k2 = convolution_block_unet(k2, 32)
+
+    u2 = crfb(p2, residual_k2, 32, 0.0625)
+    o2 = crfb(j2, residual_p2, 32, 16)
+
+    s3, p3 = encoder_block_unet(p2, 64)
+    k3, j3 = encoder_block_kinet(o1, 64)  # k3 to be used second to last
+
+    residual_p3 = convolution_block_kinet(p3, 64)
+    residual_k3 = convolution_block_unet(k3, 64)
+
+    u3 = crfb(p2, residual_k2, 32, 0.015625)
+    o3 = crfb(j2, residual_p2, 32, 64)
+
+    # b1 = convolution_block_unet(p4, 256)
+
+    # decoder block
 
     d1 = decoder_block_unet(b1, s4, 512)
     d2 = decoder_block_unet(d1, s3, 256)
