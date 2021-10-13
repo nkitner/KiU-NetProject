f0d2d698e8491de4dd03e73dc8b262d1ed2931b6
nkitner <nicolekitner@gmail.com>
2021-10-13 16:59:36 -0400

    Update add_data.py
    removed functions

--- data/add_data.py
+++ data/add_data.py
@@ -27,27 +27,6 @@ def import_data(path):
     return tensor_list
 
 
-# fxn below should be removed I think #
-def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=1):
-    '''Generates an input_function instance
-
-    Args:
-        1) data_df(df): pandas dataframe holding input data
-        2) label_df(df): pandas dataframe holding ground truth labels for the input data
-        3) num_epochs(int): epochs to train for
-        4) shuffle(bool): toggles randomization of input data
-        5) batch_size(int): batch size
-    '''
-
-    def input_function():
-        ds = tf.data.Dataset.from_tensor_slices((dict(data_df, label_df)))
-        if shuffle:
-            ds = ds.shuffle(1000)
-        ds = ds.batch(batch_size).repeat(num_epochs)
-
-    return input_function
-
-
 y_train = import_data("./training/av")
 # vessel_training_tensor = import_data("./training/vessel")
 x_train = import_data("./training/images")
