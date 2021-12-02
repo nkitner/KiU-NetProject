8e07394b18abdb1b2d4cf096abf54dbb2f44e68c
nkitner <nicolekitner@gmail.com>
2021-12-01 19:39:15 -0500

    Added Segnet and Unet to train_model

--- data/add_data.py
+++ data/add_data.py
@@ -19,14 +19,16 @@ def import_data_numpy(path):
     :return: list of Tensors
     """
     tensor_list = []
-    for file in os.listdir(path):
+    for file in sorted(os.listdir(path)):
         if file.endswith(".png") or file.endswith(".tif"):
+            # Converts to RGB because the vessel images are black and white
             png_img = Image.open(os.path.join(path, file))
-            # Encodes images as numpy arrays and adds to the list
-            tensor_list.append(np.array(png_img.getdata()).reshape((128, 128, 3)))
-        # Returns list of numpy arrays to be used in model
-    tensor_list = np.asarray(tensor_list)
-    return tensor_list
+            np_arr = normalize(np.array(png_img.getdata()).reshape((128, 128,3)))
+            # tensor = tf.convert_to_tensor(np_arr)
+            # Encodes images as tensors and adds to the list
+            tensor_list.append(np_arr)
+        # Returns list of tensors to be used in model
+    return np.asarray(tensor_list)
 
 
 def import_data_numpy_mask(path):
@@ -45,7 +47,6 @@ def import_data_numpy_mask(path):
             # Encodes images as tensors and adds to the list
             tensor_list.append(np_arr)
         # Returns list of tensors to be used in model
-    # tensor_list = np.asarray(tensor_list)
     return np.asarray(tensor_list)
 
 
--- train_model.py
+++ train_model.py
@@ -6,9 +6,11 @@ Date: 2021-10-07
 '''
 import tensorflow as tf
 from tensorflow import keras
-from model import kiunet
+from model import kiunet, unet, segnet
 from data.add_data import import_data_numpy, import_data_numpy_mask
 import numpy as np
+import custom_metrics as cm
+import datetime
 
 kiunet_model = kiunet((128, 128, 3), 2)
 
@@ -28,19 +30,61 @@ for i in range(len(y_train)):
 y_onehot_train = keras.utils.to_categorical(y_train_binary, 2)
 y_onehot_test = keras.utils.to_categorical(y_test_binary, 2)
 
-#todo - add training for Unet and SegNet
+# Create all three models
+
+# Seg-Net model
+segnet_model = segnet((128, 128, 3), 2)
+
+segnet_model.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[cm.f1_metric])
+
+# U-Net model
+unet_model = unet((128, 128, 3), 2)
+
+unet_model.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[cm.f1_metric])
+
+# KiU-Net model
+kiunet_model = kiunet((128, 128, 3), 2)
 
 kiunet_model.compile(
-    optimizer=keras.optimizers.Adam(learning_rate=0.001),  ## learning_rate is different from 3D
-    loss="binary_crossentropy",  ##maybe categorical_crossentropy if labels one-hot encoded
-    metrics=[keras.metrics.Accuracy()])  # check metric, should be 4 classes
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[cm.f1_metric])
+
+### Train each model ###
+
+# Instaniate logging for tensorboard
+segnet_train_log_dir = "logs/unet/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+segnet_train_tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=segnet_train_log_dir, histogram_freq=1)
+
+segnet_model.fit(x_train, y_onehot_train, batch_size=1, epochs=300,
+                 validation_data=(x_test, y_onehot_test), verbose=1,
+                 callbacks=[segnet_train_tensorboard_callback])
+
+segnet_model.save('segnet_model.h5', save_format='h5')
+
+# Instaniate logging for tensorboard
+unet_train_log_dir = "logs/unet/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+unet_train_tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=unet_train_log_dir, histogram_freq=1)
+
+unet_model.fit(x_train, y_onehot_train, batch_size=1, epochs=300,
+               validation_data=(x_test, y_onehot_test), verbose=1,
+               callbacks=[unet_train_tensorboard_callback])
+
+unet_model.save('unet_model.h5', save_format='h5')
+
+# Instantiate logging for tensorboard
+kiunet_train_log_dir = "logs/kiunet/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+kiunet_train_tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=kiunet_train_log_dir, histogram_freq=1)
+
 
 kiunet_model.fit(x_train, y_onehot_train, batch_size=1, epochs=300,
-                 validation_data=(x_test, y_onehot_test), verbose=1,  ## validation_data = test_data, no early stopping
-                 callbacks=keras.callbacks.TensorBoard(log_dir="./logs"))  ## log metrics in TensorBoard
+                 validation_data=(x_test, y_onehot_test), verbose=1,
+                 callbacks=[kiunet_train_tensorboard_callback])
 
-## Save using SavedModel format
-kiunet_model.save('kiUnet_model_pb')
-
-## Save again using H5 format
-kiunet_model.save('kiUnet_model2_h5', save_format='h5')
+kiunet_model.save('kiunet_model.h5', save_format='h5')
